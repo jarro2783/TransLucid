ceac236f5238194c9c9ad0dfa19c17615e8f9e64
Jarryd Beck @ koalus <jarrydb@cse.unsw.edu.au>
2010-01-28 18:31:13 +0100

    adding set types

--- src/include/tl/set_types.hpp
+++ src/include/tl/set_types.hpp
@@ -2,9 +2,15 @@
 #define SET_TYPES_HPP_INCLUDED
 
 #include <tl/builtin_types.hpp>
+#include <tl/hyperdaton.hpp>
 
 namespace TransLucid {
    class TypeAsSet : public SetBase {
+      public:
+      TypeAsSet(size_t index);
+
+      private:
+      size_t m_index;
    };
 }
 
--- src/libs/interpreter/set_types.cpp
+++ src/libs/interpreter/set_types.cpp
@@ -1,1 +1,10 @@
 #include <tl/set_types.hpp>
+
+namespace TransLucid {
+
+TypeAsSet::TypeAsSet(size_t index)
+: m_index(index)
+{
+}
+
+}
