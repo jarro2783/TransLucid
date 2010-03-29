644f403739346f4db2848945fdfc44389014c8b2
John Plaice @ lupus <plaice@cse.unsw.edu.au>
2010-03-29 15:52:15 +0200

    Added some error correction.

--- src/tests/runbinary.sh
+++ src/tests/runbinary.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+# Run a binary file, adding one or more directories temporarily to LD_LIBRARY_PATH
+
+if [ "$#" -ne "2" ]
+then
+  echo "Usage: runbinary.sh libraryPath binaryFile"
+  exit 1
+fi
 
 export LD_LIBRARY_PATH=$1:$LD_LIBRARY_PATH
 ./$2
