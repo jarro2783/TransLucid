15a9e1e9bd31fa330b448999c5d9d183d32c1267
Jarryd Beck <jarro_2783@users.sourceforge.net>
2010-05-27 09:43:35 +1000

    added factorial example properly

--- src/basic/fact1.cpp
+++ src/basic/fact1.cpp
@@ -6,10 +6,24 @@ int main(int argc, char* argv[])
 {
   TL::Translator t;
 
+  t.parse_header
+  (
+    U"dimension ustring<n>;;"
+  );
+
   t.translate_and_add_equation_set
   (
     U"fact | [n:0] = 1;;"
     U"fact = #n * (fact @ [n:#n-1]);;"
   );
+
+  TL::HD* e = t.translate_expr(U"fact @ [n:20]");
+
+  TL::TaggedConstant result = (*e)(TL::Tuple());
+
+  std::cout << result.first.value<TL::Intmp>().value() << std::endl;
+
+  delete e;
+
   return 0;
 }
