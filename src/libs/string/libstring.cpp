0240578259e0a2307fa81e6f27e2f1ee65d43607
Jarryd Beck <jarro_2783@users.sourceforge.net>
2010-10-21 17:52:37 +1100

    implementing string library

--- src/libs/string/libstring.cpp
+++ src/libs/string/libstring.cpp
@@ -1,4 +1,8 @@
 #include "libstring.hpp"
+#include <tl/fixed_indexes.hpp>
+#include <tl/equation.hpp>
+#include <tl/utility.hpp>
+#include <tl/system.hpp>
 
 namespace TransLucid
 {
@@ -6,9 +10,50 @@ namespace TransLucid
 namespace LibString
 {
 
-void
-register_string_ops(TransLucid::SystemHD& i)
+HD*
+create_plus()
 {
+}
+
+void
+register_one_op(HD* hd, SystemHD& i)
+{
+  tuple_t guard =
+  {
+    {
+      get_dimension_index(&i, U"arg0"),
+      Constant(Type(TYPE_INDEX_USTRING), TYPE_INDEX_TYPE)
+    },
+    {
+      get_dimension_index(&i, U"arg1"),
+      Constant(Type(TYPE_INDEX_USTRING), TYPE_INDEX_TYPE)
+    },
+    {
+      DIM_NAME,
+      generate_string(U"operator+")
+    }
+  };
+
+  tuple_t context =
+  {
+    {
+      DIM_VALID_GUARD,
+      Constant(Guard(GuardHD(Tuple(guard))),
+                     TYPE_INDEX_GUARD)
+    },
+    {
+      DIM_ID,
+      generate_string(U"OP")
+    }
+  };
+
+  i.addExpr(Tuple(context), hd);
+}
+
+void
+register_string_ops(SystemHD& i)
+{
+  register_one_op(create_plus(), i);
 }
 
 }
