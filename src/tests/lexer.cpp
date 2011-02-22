d0af60b197cf5b7363a89d73e0286dbe22f116d9
Jarryd Beck <jarrydb@cse.unsw.edu.au>
2011-02-22 15:42:15 +1100

    tests are starting to happen

--- src/tests/lexer.cpp
+++ src/tests/lexer.cpp
@@ -19,63 +19,155 @@ along with TransLucid; see the file COPYING.  If not see
 
 #include <tl/lexer.hpp>
 #include <tl/parser_iterator.hpp>
+#include <tl/charset.hpp>
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/home/phoenix/bind/bind_member_function.hpp>
 
 #define BOOST_TEST_MODULE lexer
 #include <boost/test/included/unit_test.hpp>
 
 namespace TL = TransLucid;
 namespace lex = TL::Parser::lex;
+namespace qi = boost::spirit::qi;
 
 BOOST_AUTO_TEST_SUITE( lexer_tests )
 
+// iterator type used to expose the underlying input stream
+typedef std::basic_string<wchar_t> wstring;
+typedef wstring::iterator base_iterator_type;
+
+// This is the token type to return from the lexer iterator
+typedef lex::lexertl::token<
+	base_iterator_type, 
+	boost::mpl::vector<mpz_class, wstring> 
+> token_type;
+
+// This is the lexer type to use to tokenize the input.
+// We use the lexertl based lexer engine.
+typedef lex::lexertl::actor_lexer<token_type> lexer_type;
+
+typedef TL::Parser::lex_tl_tokens<lexer_type> tl_lexer;
+
+// This is the iterator type exposed by the lexer 
+typedef tl_lexer::iterator_type iterator_type;
+
+enum Token
+{
+  TOKEN_IF,
+	TOKEN_FI,
+	TOKEN_WHERE,
+	TOKEN_THEN,
+	TOKEN_ELSIF,
+	TOKEN_TRUE,
+	TOKEN_FALSE
+};
+
+//the types of values that we can have
+typedef boost::variant<Token, mpz_class, wstring> Values;
+  
+//checks that the tokens are correct
+class Checker
+{
+  public:
+	Checker(const std::list<Values>& tokens)
+  : m_tokens(tokens)
+  , m_current(m_tokens.begin())
+	{
+	}
+
+	void identifier(const wstring& ws)
+	{
+    BOOST_REQUIRE(m_current != m_tokens.end());
+
+    BOOST_CHECK(ws == boost::get<wstring>(*m_current));
+
+    ++m_current;
+	}
+
+	void integer(const mpz_class& i)
+	{
+	  std::cerr << "int: " << i << std::endl;
+	}
+
+	void keyword(Token t)
+	{
+	}
+
+	private:
+  //the list of the expected tokens
+  std::list<Values> m_tokens;
+  std::list<Values>::const_iterator m_current;
+};
+
+template <typename Iterator>
+struct checker_grammar 
+  : qi::grammar<Iterator>
+{
+    template <typename TokenDef>
+    checker_grammar(TokenDef const& tok, Checker& checker)
+      : checker_grammar::base_type(start)
+      , m_checker(checker)
+    {
+      using boost::spirit::_val;
+      namespace ph = boost::phoenix;
+      using boost::spirit::_1;
+
+      start = 
+        *(ident 
+        | integer 
+        | keyword[ph::bind(&Checker::keyword, m_checker, _1)]
+        )
+      ;
+
+      ident = tok.identifier[ph::bind(&Checker::identifier, m_checker, _1)];
+      integer = tok.integer; //[ph::bind(&Checker::integer, m_checker, _1)];
+      keyword = 
+        tok.if_[_val = TOKEN_IF] 
+      | tok.fi_[_val = TOKEN_FI]
+      | tok.where_[_val = TOKEN_WHERE]
+      | tok.then_[_val = TOKEN_THEN]
+      | tok.elsif_[_val = TOKEN_ELSIF]
+      | tok.true_[_val = TOKEN_TRUE]
+      | tok.false_[_val = TOKEN_TRUE]
+      ;
+    }
+
+    qi::rule<Iterator> 
+      start
+    , ident
+    , integer
+		;
+		qi::rule<Iterator, Token()>
+      keyword
+    ;
+
+		Checker& m_checker;
+};
+
+// This is the type of the grammar to parse
+typedef checker_grammar<iterator_type> cgrammar;
+
 BOOST_AUTO_TEST_CASE ( keywords )
 {
-  typedef std::basic_string<wchar_t> wstring;
-  typedef wstring::iterator iterator_t;
-
-  typedef lex::lexertl::token<
-    iterator_t, boost::mpl::vector<mpz_class, std::string>
-  > token_type;
-
-  typedef lex::lexertl::actor_lexer<token_type> lexer_type;
-  typedef TL::Parser::lex_tl_tokens<lexer_type> tl_lexer;
-
+  Checker checker({
+    L"ifififif0a9fifi", 
+    L"testing",
+    L"hello",
+    L"world",
+    L"a",
+    L"a_b",
+    L"a5",
+    })
+  ;
   tl_lexer lexer;
+  cgrammar checkg(lexer, checker);
 
-  wstring input = L"ifififif0a9fifi  if   fi";
+  wstring input = L"ifififif0a9fifi testing hello world a a_b a5";
 
   wstring::iterator first = input.begin();
   wstring::iterator last = input.end();
 
-  lexer_type::iterator_type iter = lexer.begin(first, last);
-  lexer_type::iterator_type end = lexer.end();
-
-  if (iter == end)
-  {
-    std::cerr << "ended before it even started" << std::endl;
-  }
-
-  while (iter != end && token_is_valid(*iter))
-  {
-    std::cout << *iter << std::endl;
-    ++iter;
-  }
-
-  #if 0
-  if (iter != end)
-  {
-    std::cout << *iter;
-  }
-  else
-  {
-    std::cout << "ended too soon";
-  }
-
-  while (iter != end)
-  {
-    ++iter;
-  }
-  #endif
+	lex::tokenize_and_parse(first, last, lexer, checkg);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
