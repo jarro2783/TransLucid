fe4f4b611fdbce3e7927a0d3e91e45b09c6684d7
Jarryd Beck <jarrydb@cse.unsw.edu.au>
2011-06-06 16:55:50 +1000

    added Constant2 with detail
    getting reading to do the big change that will break for 2 days to change how
    Constant works

---
+++ src/include/tl/detail/types_detail.hpp
@@ -0,0 +1,51 @@
+/* Forward declaration for types.
+   Copyright (C) 2011 Jarryd Beck and John Plaice
+
+This file is part of TransLucid.
+
+TransLucid is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3, or (at your option)
+any later version.
+
+TransLucid is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with TransLucid; see the file COPYING.  If not see
+<http://www.gnu.org/licenses/>.  */
+
+#ifndef TL_TYPES_DETAIL_HPP
+#define TL_TYPES_DETAIL_HPP
+
+#include <tl/types.hpp>
+
+namespace TransLucid
+{
+  namespace detail
+  {
+    template <>
+    struct set_constant_func<int8_t>
+    {
+      void
+      operator()(Constant2& c, int8_t v)
+      {
+        c.si8 = v;
+      }
+    };
+
+    template <>
+    struct get_constant_func<int8_t>
+    {
+      int8_t
+      operator()(Constant2& c)
+      {
+        return c.si8;
+      }
+    };
+  }
+}
+
+#endif
---
+++ src/include/tl/types_fwd.hpp
@@ -0,0 +1,37 @@
+/* Forward declaration for types.
+   Copyright (C) 2011 Jarryd Beck and John Plaice
+
+This file is part of TransLucid.
+
+TransLucid is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3, or (at your option)
+any later version.
+
+TransLucid is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with TransLucid; see the file COPYING.  If not see
+<http://www.gnu.org/licenses/>.  */
+
+#ifndef TL_TYPES_FWD_HPP
+#define TL_TYPES_FWD_HPP
+
+namespace TransLucid
+{
+  class Constant2;
+
+  namespace detail
+  {
+    template <typename t>
+    struct set_constant_func;
+
+    template <typename t>
+    struct get_constant_func;
+  }
+}
+
+#endif
