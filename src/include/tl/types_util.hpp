af9c6b4af0f9d6ea347de61758b3ab19d338f145
Jarryd Beck <jarrydb@cse.unsw.edu.au>
2011-06-08 15:35:49 +1000

    added missing

---
+++ src/include/tl/types_util.hpp
@@ -0,0 +1,46 @@
+/* Types utility functions.
+   Copyright (C) 2011 Jarryd Beck and John Plaice
+
+This file is part of TransLucid.
+
+TransLucid is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3, or (at your option)
+any later version.
+
+TransLucid is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with TransLucid; see the file COPYING.  If not see
+<http://www.gnu.org/licenses/>.  */
+
+#ifndef TL_TYPES_UTIL_HPP_INCLUDED
+#define TL_TYPES_UTIL_HPP_INCLUDED
+
+#include <tl/types.hpp>
+
+namespace TransLucid
+{
+  template <typename T>
+  Constant
+  make_constant_pointer(const T& v, TypeFunctions* funs, type_index index)
+  {
+    std::unique_ptr<T> value(detail::clone<T>()(v));
+    ConstantPointerValue* p = 
+      new ConstantPointerValue(funs, value.get());
+    value.release();
+    return Constant(p, index);
+  }
+
+  template <typename T>
+  const T&
+  get_constant_pointer(const Constant& c)
+  {
+    return *static_cast<T*>(c.data.ptr->data);
+  }
+}
+
+#endif
