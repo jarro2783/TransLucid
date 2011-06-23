282e8cbec7f6bf6531018a0c6a87da2926562a59
Jarryd Beck <jarrydb@cse.unsw.edu.au>
2011-06-23 18:31:03 +1000

    added missing

---
+++ src/tltext/demandhd.cpp
@@ -0,0 +1,64 @@
+/* tltext demand hyperdaton
+   Copyright (C) 2011 Jarryd Beck and John Plaice
+
+This file is part of TransLucid.
+
+TransLucid is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3, or (at your option)
+any later version.
+
+TransLucid is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with TransLucid; see the file COPYING.  If not see
+<http://www.gnu.org/licenses/>.  */
+
+#include <tl/types_util.hpp>
+
+#include "demandhd.hpp"
+
+namespace TransLucid
+{
+
+namespace TLText
+{
+
+DemandHD::DemandHD(DimensionRegistry& dims)
+: OutputHD(1)
+{
+  m_slot = dims.getDimensionIndex(U"slot");
+}
+
+void
+DemandHD::put(const Tuple& k, const Constant& c)
+{
+  auto iter = k.find(m_slot);
+
+  if (iter == k.end())
+  {
+    //error
+  }
+  else
+  {
+    size_t slot = get_constant_pointer<mpz_class>(iter->second).get_ui();
+
+    std::cerr << "stuffing into slot " << slot << std::endl;
+
+    if (m_results.size() <= slot)
+    {
+      //let's just allocate twice as much because the most common case is
+      //probably filling up in slots 1, 2, 3...
+      m_results.resize((slot+1)*2);
+    }
+
+    m_results.at(slot) = c;
+  }
+}
+
+} //namespace TLText
+
+} //namespace TransLucid
---
+++ src/tltext/demandhd.hpp
@@ -0,0 +1,55 @@
+/* tltext demand hyperdaton
+   Copyright (C) 2011 Jarryd Beck and John Plaice
+
+This file is part of TransLucid.
+
+TransLucid is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3, or (at your option)
+any later version.
+
+TransLucid is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with TransLucid; see the file COPYING.  If not see
+<http://www.gnu.org/licenses/>.  */
+
+#ifndef TLTEXT_DEMAND_HD
+#define TLTEXT_DEMAND_HD
+
+#include <tl/hyperdaton.hpp>
+#include <tl/registries.hpp>
+
+#include <vector>
+
+namespace TransLucid
+{
+  namespace TLText
+  {
+    class DemandHD : public OutputHD
+    {
+      public:
+
+      DemandHD(DimensionRegistry& dims);
+
+      void
+      put(const Tuple& k, const Constant& c);
+
+      const Constant&
+      operator()(size_t i)
+      {
+        return m_results.at(i);
+      }
+
+      private:
+
+      std::vector<Constant> m_results;
+      dimension_index m_slot;
+    };
+  }
+}
+
+#endif
