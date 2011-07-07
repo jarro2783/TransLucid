cf76104f53658fe9fcb2b1ff984021e919284904
Jarryd Beck <jarrydb@cse.unsw.edu.au>
2011-07-07 15:33:06 +1000

    added internal strings file

---
+++ src/libs/system/internal_strings.cpp
@@ -0,0 +1,25 @@
+/* Predefined strings used internally.
+   Copyright (C) 2011 Jarryd Beck
+
+This file is part of TransLucid.
+
+TransLucid is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3, or (at your option)
+any later version.
+
+TransLucid is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with TransLucid; see the file COPYING.  If not see
+<http://www.gnu.org/licenses/>.  */
+
+#include <tl/types.hpp>
+
+namespace TransLucid
+{
+  u32string type_name;
+}
