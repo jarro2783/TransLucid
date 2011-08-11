e6b5d3891427b4e2bce71b323c46a5aa161f953a
Jarryd Beck <jarrydb@cse.unsw.edu.au>
2011-08-11 13:28:18 +1000

    file docs

--- src/libs/system/tree_to_wstree.cpp
+++ src/libs/system/tree_to_wstree.cpp
@@ -17,6 +17,10 @@ You should have received a copy of the GNU General Public License
 along with TransLucid; see the file COPYING.  If not see
 <http://www.gnu.org/licenses/>.  */
 
+/** @file tree_to_wstree.cpp
+ * Rewrites an expression tree to remove operators and literals.
+ */
+
 #include <tl/tree_to_wstree.hpp>
 #include <tl/internal_strings.hpp>
 
