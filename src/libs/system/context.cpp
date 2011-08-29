d4fff2ab957969e339b18f58755c676d02132c5c
Jarryd Beck <jarrydb@cse.unsw.edu.au>
2011-08-29 19:16:27 +1000

    added context.cpp

--- src/libs/system/context.cpp
+++ src/libs/system/context.cpp
@@ -18,51 +18,24 @@ along with TransLucid; see the file COPYING.  If not see
 <http://www.gnu.org/licenses/>.  */
 
 /**
- * @file context.hpp
+ * @file context.cpp
  * The context object.
  */
 
-#ifndef TL_CONTEXT_HPP_INCLUDED
-#define TL_CONTEXT_HPP_INCLUDED
-
-#include <tl/types.hpp>
-
-#include <deque>
-#include <stack>
+#include <tl/context.hpp>
 
 namespace TransLucid
 {
-  class Context
-  {
-    public:
 
-    Context();
-
-    void
-    perturb(const Tuple& t);
-
-    /**
-     * Restores to the previous context.
-     * Pops one value from every dimension that exists in t.
-     * @param t The tuple to restore from.
-     */
-    void 
-    restore(const Tuple& t);
-
-    Constant
-    lookup(dimension_index dim);
-
-    private:
-
-    typedef std::deque<std::stack<Constant>> ContextType;
-
-    dimension_index m_min;
-    dimension_index m_max;
-
-    Constant m_all;
-
-    ContextType m_context;
-  };
+Context::Context()
+: m_min(0), m_max(0)
+{
 }
 
-#endif
+Constant
+Context::lookup(dimension_index dim)
+{
+  return m_all;
+}
+
+}
