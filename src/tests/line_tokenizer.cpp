aea27957ce1ebcedaca357b4017e75aeee8e4ac5
Jarryd Beck <jarrydb@cse.unsw.edu.au>
2011-09-21 13:29:20 +1000

    added a where clause test

--- src/tests/line_tokenizer.cpp
+++ src/tests/line_tokenizer.cpp
@@ -143,18 +143,41 @@ BOOST_AUTO_TEST_CASE( percent )
 
 BOOST_AUTO_TEST_CASE( where )
 {
-  std::string input = "x where var x = 5;; end;;";
+  {
+    std::string input = "x where var x = 5;; end;;";
 
-  TL::Parser::U32Iterator iter(
-    TL::Parser::makeUTF8Iterator(input.begin()),
-    TL::Parser::makeUTF8Iterator(input.end())
-  );
+    TL::Parser::U32Iterator iter(
+      TL::Parser::makeUTF8Iterator(input.begin()),
+      TL::Parser::makeUTF8Iterator(input.end())
+    );
 
-  TL::LineTokenizer tokenize(iter);
+    TL::LineTokenizer tokenize(iter);
 
-  auto n = tokenize.next();
-  BOOST_CHECK_EQUAL(n.first, TL::LineType::LINE);
-  BOOST_CHECK_EQUAL(n.second, TL::to_u32string(input));
+    auto n = tokenize.next();
+    BOOST_CHECK_EQUAL(n.first, TL::LineType::LINE);
+    BOOST_CHECK_EQUAL(n.second, TL::to_u32string(input));
+  }
+
+  {
+    std::string input = 
+R"(var f = x + #d where
+   var x = 5;;
+   dim d <- 4;;
+end;;)";
+
+    
+    TL::Parser::U32Iterator iter(
+      TL::Parser::makeUTF8Iterator(input.begin()),
+      TL::Parser::makeUTF8Iterator(input.end())
+    );
+
+    TL::LineTokenizer tokenize(iter);
+
+    auto n = tokenize.next();
+    BOOST_CHECK_EQUAL(n.first, TL::LineType::LINE);
+    BOOST_CHECK_EQUAL(n.second, TL::to_u32string(input));
+  }
+
 }
 
 BOOST_AUTO_TEST_SUITE_END()
