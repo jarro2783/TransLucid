043906354ab8ba819f3c5e29f78c11f9fbb0f5e6
Jarryd Beck <jarrydb@cse.unsw.edu.au>
2011-10-03 11:16:21 +1100

    basic wilfred almost works

--- src/tltext/wilfred.cpp
+++ src/tltext/wilfred.cpp
@@ -29,6 +29,55 @@ along with TransLucid; see the file COPYING.  If not see
 //i/ostringstream
 //then output appropriately
 
+// Parse character as hex digit.
+// Returns -1 on error.
+int hex2dec(char hex) {
+    if (hex >= '0' && hex <= '9')
+        return hex - '0';
+    if (hex >= 'A' && hex <= 'F')
+        return 10 + hex - 'A';
+    if (hex >= 'a' && hex <= 'f')
+        return 10 + hex - 'a';
+    return -1;
+}
+
+// Return a URI-unescaped version of s.
+// Unparseable %xx sequences are dropped.
+std::string unescape(const std::string& s) 
+{
+  std::string result;
+  result.reserve(s.length());
+  for 
+  (
+    std::string::const_iterator it = s.begin();
+    it != s.end();
+    it++
+  ) 
+  {
+    if (*it == '%') 
+    {
+      if (s.end() - it >= 2) 
+      {
+        int a = hex2dec(*++it);
+        int b = hex2dec(*++it);
+        if (a >= 0 && b >= 0)
+        {
+          result += (char)(a * 16 + b);
+        }
+      }
+    } 
+    else if (*it == '+') 
+    {
+        result += ' ';
+    } 
+    else
+    {
+      result += *it;
+    }
+  }
+  return result;
+}
+
 int main(int argc, char* argv[])
 {
   std::cout << "Content-type: text/html\r\n\r\n";
@@ -51,12 +100,37 @@ int main(int argc, char* argv[])
 
   std::cout << inbuf.get() << std::endl;
 
+  std::map<std::string, std::string> invars;
+
+  while (!input.eof())
+  {
+    std::string var;
+    std::string data;
+    std::getline(input, var, '=');
+    std::getline(input, data, '&');
+
+    invars.insert(std::make_pair(var, unescape(data)));
+    std::cout << "<h1>" << var << "</h1>" 
+      << std::endl << unescape(data) << std::endl;
+  }
+
+  std::cout << "<h1>Output</h1>" << std::endl;
+
+  auto prog = invars.find("program");
+  if (prog == invars.end())
+  {
+    std::cout << "No program specified" << std::endl;
+    exit(1);
+  }
+
+  TransLucid::TLText::TLText tl;
+
+  std::istringstream progstream(prog->second);
+  tl.set_input(&progstream);
+
+  tl.run();
+
   std::cout << "</body></html>" << std::endl;
-
-  //std::vector<std::pair<std::string, std::string>> invars;
-
-  //std::string var;
-  //std::getline(input, var, '=');
 
   return 0;
 }
