2bdff4654bd1ebb577d6aa2fbb81b7b51e54b926
Jarryd Beck <jarrydb@cse.unsw.edu.au>
2011-11-01 22:36:50 +1100

    tree printer is happening

--- src/libs/system/tree_printer_new.cpp
+++ src/libs/system/tree_printer_new.cpp
@@ -1,7 +1,9 @@
 #include <tl/tree_printer.hpp>
 #include <tl/tree_old_to_new.hpp>
-#include <sstream>
 #include <tl/output.hpp>
+
+#include <sstream>
+#include <stack>
 
 namespace TransLucid
 {
@@ -10,6 +12,66 @@ class TreePrinterNew
 {
   private:
   std::ostringstream m_os;
+
+  enum class Subtree
+  {
+    LEFT,
+    NONE,
+    RIGHT
+  };
+
+  enum class Assoc
+  {
+    LEFT,
+    NON,
+    RIGHT
+  };
+
+  enum class Precedence
+  {
+    MINUS_INF,
+    FN_ABSTRACTION,
+    WHERE_CLAUSE,
+    BINARY_FN,
+    FN_APP,
+    PREFIX_FN,
+    POSTFIX_FN
+  };
+
+  class ExprPrecedence
+  {
+    public:
+    ExprPrecedence(int a)
+    : m_a(a), m_b(0)
+    {
+    }
+
+    ExprPrecedence(int a, const mpz_class& b)
+    : m_a(a), m_b(b)
+    {
+    }
+
+    bool
+    operator<(const ExprPrecedence& rhs) const
+    {
+      return (m_a < rhs.m_a) || (m_a == rhs.m_a && m_b < rhs.m_b);
+    }
+
+    private:
+    int m_a;
+
+    //this allows an infinite number of precedences inside the same
+    //m_a precedence
+    //although really it allows binary operators to work
+    mpz_class m_b;
+  };
+
+  //no attributes in visitation, so we have to keep our own stack
+  //this means that every function which pushes to one of the stacks
+  //MUST pop it when it exits
+  std::stack<ExprPrecedence> m_precedence;
+  std::stack<Assoc> m_assoc;
+  std::stack<Subtree> m_subtree;
 
   public:
   typedef void result_type;
@@ -26,10 +88,77 @@ class TreePrinterNew
     m_os << "not implemented";
   }
 
+  void 
+  operator()(bool b)
+  {
+    m_os << std::boolalpha << b;
+  }
+
+  void
+  operator()(Special s)
+  {
+  }
+
+  void
+  operator()(char32_t c)
+  {
+  }
+
+  void
+  operator()(const u32string& s)
+  {
+    //probably need to escape this or something, we need to work out
+    //what this all means
+    m_os << "\"" << s << "\"";
+  }
+
+  void 
+  operator()(const TreeNew::LiteralExpr& l)
+  {
+    m_os << l.type;
+    operator()(l.text);
+  }
+
+  void
+  operator()(const TreeNew::DimensionExpr& d)
+  {
+    if (d.text.size() == 0)
+    {
+      m_os << "dim_" << d.dim;
+    }
+    else
+    {
+      m_os << d.text;
+    }
+  }
+
   void
   operator()(const TreeNew::IdentExpr& ident)
   {
     m_os << ident.text;
+  }
+
+  void
+  operator()(const TreeNew::HashSymbol&)
+  {
+    m_os << "#";
+  }
+
+  void
+  operator()(const TreeNew::ParenExpr& p)
+  {
+    //pass on the paren info without modification
+    apply_visitor(*this, p.e);
+  }
+
+  void
+  operator()(const TreeNew::UnaryOpExpr& u)
+  {
+  }
+
+  void
+  operator()(const TreeNew::BinaryOpExpr& u)
+  {
   }
 };
 
