3e6aac7ff2f5667319e69b2f3108ac780fa654e0
Jarryd Beck <jarrydb@cse.unsw.edu.au>
2011-11-11 08:18:21 +1100

    made uuid tester awesome

--- src/tests/uuid.cpp
+++ src/tests/uuid.cpp
@@ -1,6 +1,8 @@
 #include <random>
 #include <iostream>
 #include <iomanip>
+#include <algorithm>
+#include <unordered_set>
 
 class RandomIterator
 {
@@ -47,23 +49,62 @@ class RandomIterator
 
 class uuid
 {
-  static constexpr size_t static_size() { return 16; }
+  static const size_t static_size = 16;
   public:
   typedef uint8_t* iterator;
   typedef const uint8_t* const_iterator;
 
   iterator begin() { return data; }
-  iterator end() { return data + static_size(); }
+  iterator end() { return data + static_size; }
   const_iterator begin() const { return data; }
-  const_iterator end() const { return data + static_size(); }
+  const_iterator end() const { return data + static_size; }
 
-  constexpr size_t size() { return static_size(); }
+  constexpr size_t size() { return static_size; }
 
   private:
   //gcc bug?
-  //uint8_t data[static_size()];
-  uint8_t data[16];
+  uint8_t data[static_size];
 };
+
+inline bool operator==(const uuid& lhs, const uuid& rhs)
+{
+  return std::equal(lhs.begin(), lhs.end(), rhs.begin());
+}
+
+inline bool operator<(const uuid& lhs, const uuid& rhs)
+{
+  return std::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), 
+    rhs.end());
+}
+
+inline bool operator!=(const uuid& lhs, const uuid& rhs)
+{
+  return !(lhs == rhs);
+}
+
+inline std::size_t hash_value(uuid const& u) /* throw() */
+{
+    std::size_t seed = 0;
+    for(uuid::const_iterator i=u.begin(); i != u.end(); ++i)
+    {
+        seed ^= static_cast<std::size_t>(*i) + 
+          0x9e3779b9 + (seed << 6) + (seed >> 2);
+    }
+
+    return seed;
+}
+
+namespace std
+{
+  template <>
+  struct hash<uuid>
+  {
+    size_t operator()(const uuid& u) const
+    {
+      return hash_value(u);
+    }
+  };
+}
 
 template <typename Generator>
 void
@@ -102,13 +143,11 @@ class uuid_random_generator
 
     int i = 0;
     unit_type random = generator();
-    std::cerr << "random number = " << std::hex << random << std::endl;
     for (auto it = u.begin(); it != u.end(); ++it, ++i)
     {
       if (i == sizeof(unit_type))
       {
         random = generator();
-        std::cerr << "random number = " << std::hex << random << std::endl;
         i = 0;
       }
 
@@ -122,23 +161,117 @@ class uuid_random_generator
   full_bits_generator generator;
 };
 
+std::ostream&
+operator<<(std::ostream& os, const uuid& id)
+{
+  os << std::setfill('0');
+  for (auto it = id.begin(); it != id.end(); ++it)
+  {
+    os << std::setw(2) << std::hex << static_cast<size_t>(*it);
+  }
+  return os;
+}
+
 typedef uuid_random_generator<std::mt19937> random_generator;
 
-int main()
+void collision_test(int n)
 {
   random_generator gen;
 
-  for (int i = 0; i != 1000; ++i)
-  {
-    uuid u = gen();
+  int tick = n / 100;
 
-    std::cout << std::setfill('0');
-    for (auto it = u.begin(); it != u.end(); ++it)
+  if (tick == 0)
+  {
+    tick = 1;
+  }
+
+  std::unordered_set<uuid> uuid_set;
+
+  int collisions = 0;
+
+  std::unordered_set<uuid> collision_set;
+
+  for (int i = 0; i != n; ++i)
+  {
+    if (i % tick == 0)
     {
-      std::cout << std::setw(2) << std::hex << static_cast<size_t>(*it);
+      std::cout << "." << std::flush;
     }
 
-    std::cout << std::endl;
+    uuid u = gen();
+
+    if (!uuid_set.insert(u).second)
+    {
+      std::cout << "x" << std::endl;
+      ++collisions;
+      collision_set.insert(u);
+    }
+
+  }
+
+  std::cout << std::endl;
+
+  if (collisions == 0)
+  {
+    std::cout << "there were no collisions" << std::endl;
+  }
+  else if (collisions == 1)
+  {
+    std::cout << "there was one collision" << std::endl;
+  }
+  else if (collisions >= 2)
+  {
+    std::cout << "there were " << collisions << "collisions" << std::endl;
+  }
+
+  for (auto id : collision_set)
+  {
+    std::cout << id << std::endl;
+  }
+}
+
+void
+generate(int n)
+{
+  random_generator gen;
+  for (int i = 0; i != n; ++i)
+  {
+    std::cout << gen() << std::endl;
+  }
+}
+
+void
+usage(char* prog)
+{
+  std::cout << "TransLucid uuid tester" << std::endl << std::endl;
+  std::cout << prog << "[options]" << std::endl << std::endl;
+  std::cout << "Program options:" << std::endl;
+  std::cout <<
+  "  --collision-test N         run a collision test with N uuids generated\n"
+  "  --generate N               output N uuids to stdout\n"
+  ;
+}
+
+int main(int argc, char* argv[])
+{
+  if (argc != 3)
+  {
+    usage(argv[0]);
+    exit(1);
+  }
+
+  if (argv[1] == std::string("--collision-test"))
+  {
+    collision_test(std::atoi(argv[2]));
+  }
+  else if (argv[1] == std::string("--generate"))
+  {
+    generate(std::atoi(argv[2]));
+  }
+  else
+  {
+    usage(argv[0]);
+    exit(1);
   }
 
   return 0;
