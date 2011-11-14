09a42e1c8a7a4dfe90a422400486371868eb4dc8
Jarryd Beck <jarrydb@cse.unsw.edu.au>
2011-11-15 09:49:21 +1100

    moved tests that don't work yet
    getting the distribution to pass distcheck

--- src/libs/system/tree_printer.cpp
+++ src/libs/system/tree_printer.cpp
@@ -1,9 +1,28 @@
-#include <tl/tree_printer.hpp>
-#include <tl/output.hpp>
-#include <tl/parser_api.hpp>
+/* Tree printer.
+   Copyright (C) 2011 Jarryd Beck
+
+This file is part of TransLucid.
+
+TransLucid is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3, or (at your option)
+any later version.
+
+TransLucid is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with TransLucid; see the file COPYING.  If not see
+<http://www.gnu.org/licenses/>.  */
 
 #include <sstream>
 #include <stack>
+
+#include <tl/tree_printer.hpp>
+#include <tl/output.hpp>
+#include <tl/parser_api.hpp>
 
 namespace TransLucid
 {
