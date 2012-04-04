97ead32c20439d3f3fc368ae205f6e7e7122754f
Jarryd Beck <jarrydb@cse.unsw.edu.au>
2012-04-04 11:36:24 +1000

    add cache tests

---
+++ src/tests/cache.cpp
@@ -0,0 +1,34 @@
+/* Cache tests.
+   Copyright (C) 2012 Jarryd Beck
+
+This file is part of TransLucid.
+
+TransLucid is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3, or (at your option)
+any later version.
+
+TransLucid is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with TransLucid; see the file COPYING.  If not see
+<http://www.gnu.org/licenses/>.  */
+
+/**
+ * @file iterator.cpp
+ * Iterator tests.
+ */
+
+#include <tl/cache.hpp>
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+namespace TL = TransLucid;
+
+TEST_CASE ( "upon use case", "mimic how upon uses the cache" )
+{
+}
