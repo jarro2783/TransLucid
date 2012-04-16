722d6f6f3b632f661e605ec55dfba1150ec37433
Jarryd Beck <jarrydb@cse.unsw.edu.au>
2012-04-16 22:14:44 +1000

    shuffle around fixed number for testing

--- src/libs/system/types/integer.cpp
+++ src/libs/system/types/integer.cpp
@@ -19,101 +19,15 @@ along with TransLucid; see the file COPYING.  If not see
 
 #include <tl/fixed_indexes.hpp>
 #include <tl/system_util.hpp>
+#include <tl/types/fixed_number.hpp>
 #include <tl/types/function.hpp>
 #include <tl/types/integer.hpp>
 #include <tl/types/string.hpp>
 
-#include <functional>
-#include <sstream>
-
 namespace TransLucid
 {
-
 namespace
 {
-
-template <typename T>
-class FixedInteger
-{
-  public:
-
-  void
-  init(System& s, const u32string& name)
-  {
-    m_index = s.getTypeIndex(name);
-    m_typename = name;
-
-    makeEquation(s, U"construct_" + name, &FixedInteger::construct);
-    makeEquation(s, U"print_" + name, &FixedInteger::print);
-
-    addPrinter(s, name, U"print_" + name); 
-
-    addTypeEquation(s, name);
-  }
-
-  Constant
-  construct(const Constant& c)
-  {
-    T value;
-
-    if (c.index() != TYPE_INDEX_USTRING)
-    {
-      return Types::Special::create(SP_UNDEF);
-    }
-
-    const u32string& s = Types::String::get(c);
-
-    std::istringstream is(utf32_to_utf8(s));
-    is >> value;
-
-    return Constant(value, m_index);
-  }
-
-  Constant
-  print(const Constant& c)
-  {
-    T value = get_constant<T>(c);
-
-    std::ostringstream os;
-    os << value;
-    return Types::String::create(u32string(os.str().begin(), os.str().end()));
-  }
-
-  private:
-
-  typedef 
-  std::function<Constant(const Constant&)>
-  UnaryFunction;
-
-  template <typename Ret, typename... Args>
-  void
-  makeEquation
-  (
-    System& s, 
-    const u32string& name, 
-    Ret (FixedInteger<T>::*f)(Args...)
-  )
-  {
-    std::unique_ptr<BuiltinBaseFunction<sizeof...(Args)>> 
-      fn(new BuiltinBaseFunction<sizeof...(Args)>
-      (createUnaryFunction(&FixedInteger::construct))
-    );
-    std::unique_ptr<BangAbstractionWS> ws(new BangAbstractionWS(fn.get()));
-    fn.release();
-    s.addEquation(name, ws.get());
-    ws.release();
-  }
-
-  UnaryFunction
-  createUnaryFunction(Constant (FixedInteger<T>::*f)(const Constant&))
-  {
-    using std::placeholders::_1;
-    return std::bind(std::mem_fun(f), this, _1);
-  }
-
-  type_index m_index;
-  u32string m_typename;
-};
 
 FixedInteger<int8_t> s8;
 
