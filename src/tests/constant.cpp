e914e43e52efd10aa55276556c243f6b74ae7f7e
Jarryd Beck <jarrydb@cse.unsw.edu.au>
2012-04-16 12:53:18 +1000

    rename some tests

--- src/tests/constant.cpp
+++ src/tests/constant.cpp
@@ -40,7 +40,7 @@ namespace TL = TransLucid;
 
 template <typename T>
 void
-test_constant(T&& v, TL::type_index ti)
+test_get_constant(T&& v, TL::type_index ti)
 {
   TL::Constant c(std::forward<T>(v), ti);
 
@@ -49,16 +49,16 @@ test_constant(T&& v, TL::type_index ti)
 }
 
 void
-test_bool()
+test_get_bool()
 {
   TL::type_index ti = TL::TYPE_INDEX_BOOL;
-  test_constant(true, ti);
-  test_constant(false, ti);
+  test_get_constant(true, ti);
+  test_get_constant(false, ti);
 }
 
 template <typename T>
 void
-test_integer(TL::type_index ti)
+test_get_integer(TL::type_index ti)
 {
   //test 100 values in the range of the type
 
@@ -81,59 +81,59 @@ test_integer(TL::type_index ti)
   T value = min;
   while (i != 100)
   {
-    test_constant(value, ti);
+    test_get_constant(value, ti);
     value += step;
     ++i;
   }
 }
 
 void
-test_character()
+test_get_character()
 {
   //at least test ascii
   char32_t c = 1;
 
   for (; c != 0x7F; ++c)
   {
-    test_constant(c, TL::TYPE_INDEX_UCHAR);
+    test_get_constant(c, TL::TYPE_INDEX_UCHAR);
   }
 
   //then test bigger ones to make sure they can be stored
-  test_constant(0xA1, TL::TYPE_INDEX_UCHAR);
-  test_constant(0xA1, TL::TYPE_INDEX_UCHAR);
-  test_constant(0xA1, TL::TYPE_INDEX_UCHAR);
-  test_constant(0x1A6, TL::TYPE_INDEX_UCHAR);
-  test_constant(0x800, TL::TYPE_INDEX_UCHAR);
-  test_constant(0x10AC, TL::TYPE_INDEX_UCHAR);
-  test_constant(0x10400, TL::TYPE_INDEX_UCHAR);
-  test_constant(0x100518, TL::TYPE_INDEX_UCHAR);
+  test_get_constant(0xA1, TL::TYPE_INDEX_UCHAR);
+  test_get_constant(0xA1, TL::TYPE_INDEX_UCHAR);
+  test_get_constant(0xA1, TL::TYPE_INDEX_UCHAR);
+  test_get_constant(0x1A6, TL::TYPE_INDEX_UCHAR);
+  test_get_constant(0x800, TL::TYPE_INDEX_UCHAR);
+  test_get_constant(0x10AC, TL::TYPE_INDEX_UCHAR);
+  test_get_constant(0x10400, TL::TYPE_INDEX_UCHAR);
+  test_get_constant(0x100518, TL::TYPE_INDEX_UCHAR);
 }
 
 template <typename T>
 void
-test_float(TL::type_index ti)
+test_get_float(TL::type_index ti)
 {
-  test_constant<T>(1, ti);
-  test_constant<T>(1.5, ti);
-  test_constant<T>(2.5, ti);
-  test_constant<T>(100.6, ti);
+  test_get_constant<T>(1, ti);
+  test_get_constant<T>(1.5, ti);
+  test_get_constant<T>(2.5, ti);
+  test_get_constant<T>(100.6, ti);
 }
 
 TEST_CASE ( "get set", "get_constant and set_constant work for all types" )
 {
-  test_bool();
+  test_get_bool();
 
-  test_integer<int8_t>(TI_UI8);
-  test_integer<uint8_t>(TI_SI8);
-  test_integer<int16_t>(TI_UI16);
-  test_integer<uint16_t>(TI_SI16);
-  test_integer<int32_t>(TI_UI32);
-  test_integer<uint32_t>(TI_SI32);
-  test_integer<int64_t>(TI_UI64);
-  test_integer<uint64_t>(TI_SI64);
+  test_get_integer<int8_t>(TI_UI8);
+  test_get_integer<uint8_t>(TI_SI8);
+  test_get_integer<int16_t>(TI_UI16);
+  test_get_integer<uint16_t>(TI_SI16);
+  test_get_integer<int32_t>(TI_UI32);
+  test_get_integer<uint32_t>(TI_SI32);
+  test_get_integer<int64_t>(TI_UI64);
+  test_get_integer<uint64_t>(TI_SI64);
 
-  test_character();
+  test_get_character();
 
-  test_float<float>(TI_FLOAT);
-  test_float<double>(TI_DOUBLE);
+  test_get_float<float>(TI_FLOAT);
+  test_get_float<double>(TI_DOUBLE);
 }
