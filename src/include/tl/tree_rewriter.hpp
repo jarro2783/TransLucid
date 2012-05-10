6c2cac8b3a7aa1009d9e70646ecd6cb8dd31029f
Jarryd Beck <jarrydb@cse.unsw.edu.au>
2012-05-10 14:52:04 +1000

    adding new tree walking files

---
+++ src/include/tl/tree_rewriter.hpp
@@ -0,0 +1,32 @@
+/* Rewrites concrete syntax
+   Copyright (C) 2012 Jarryd Beck
+
+This file is part of TransLucid.
+
+TransLucid is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3, or (at your option)
+any later version.
+
+TransLucid is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with TransLucid; see the file COPYING.  If not see
+<http://www.gnu.org/licenses/>.  */
+
+/**
+ * @file tree_rewriter.hpp
+ * Rewrites expression trees.
+ */
+
+#ifndef TL_TREE_REWRITER_HPP_INCLUDED
+#define TL_TREE_REWRITER_HPP_INCLUDED
+
+namespace TransLucid
+{
+}
+
+#endif
