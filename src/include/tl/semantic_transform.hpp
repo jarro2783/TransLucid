22b024a4677c8e544daedc4804077756dabaee51
Jarryd Beck <jarrydb@cse.unsw.edu.au>
2012-05-13 16:53:38 +1000

    adding semantic transform stuff

--- src/include/tl/semantic_transform.hpp
+++ src/include/tl/semantic_transform.hpp
@@ -21,8 +21,61 @@ along with TransLucid; see the file COPYING.  If not see
  * @file semantic_transform.hpp
  * Transforms expression trees according to the semantics in the paper:
  * Higher-order multidimensional programming
+ * The input tree must:
+ * 1. Have names renamed so that they are unique,
+ * 2. Have free variables marked in functions
+ *
+ * The output is a tree rewritten according to the transformation. Any extra
+ * equations added by where clauses and functions are recorded in a list.
  */
+
+#ifndef TL_SEMANTIC_TRANSFORM_HPP_INCLUDED
+#define TL_SEMANTIC_TRANSFORM_HPP_INCLUDED
+
+#include <tl/ast.hpp>
+#include <tl/generic_walker.hpp>
 
 namespace TransLucid
 {
+  class System;
+
+  class SemanticTransform : public GenericTreeWalker<SemanticTransform>
+  {
+    public:
+    typedef Tree::Expr result_type;
+
+    using GenericTreeWalker::operator();
+
+    SemanticTransform(System& system)
+    : m_system(system)
+    {
+    }
+
+    Tree::Expr
+    transform(const Tree::Expr& e);
+
+    Tree::Expr
+    operator()(const Tree::WhereExpr& where);
+
+    Tree::Expr
+    operator()(const Tree::LambdaExpr& e);
+
+    Tree::Expr
+    operator()(const Tree::PhiExpr& e);
+
+    private:
+    System& m_system;
+
+    std::vector<dimension_index> m_Lout;
+    std::vector<dimension_index> m_Lin;
+
+    std::vector<Parser::Equation> m_newVars;
+
+    //we actually only need one scope stack
+    std::vector<dimension_index> m_scope;
+
+    std::vector<u32string> m_scopeNames;
+  };
 }
+
+#endif
---
+++ src/libs/system/semantic_transform.cpp
@@ -0,0 +1,168 @@
+/* Rewrites trees according to the semantics transformation
+   Copyright (C) 2011,2012 Jarryd Beck
+
+This file is part of TransLucid.
+
+TransLucid is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3, or (at your option)
+any later version.
+
+TransLucid is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with TransLucid; see the file COPYING.  If not see
+<http://www.gnu.org/licenses/>.  */
+
+#include <tl/semantic_transform.hpp>
+#include <tl/system.hpp>
+
+namespace TransLucid
+{
+
+Tree::Expr
+SemanticTransform::operator()(const Tree::WhereExpr& e)
+{
+  //all the names are already unique
+  Tree::WhereExpr w = e;
+
+  std::vector<dimension_index> myLin;
+  
+  //generate new label
+  dimension_index label = m_system.nextHiddenDim();
+  w.myDim = label;
+
+  //store L_out
+  //visit children variables
+  m_Lout.push_back(label);
+
+  //L_in is all of the inner where clauses 
+  //apply_visitor(*this, e.e);
+
+  m_Lin.clear();
+  for (const auto& evar : e.vars)
+  {
+    //expr
+    m_Lin.clear();
+    Tree::Expr varExpr = apply_visitor(*this, std::get<3>(evar));
+    myLin.insert(myLin.end(), m_Lin.begin(), m_Lin.end());
+
+    //guard
+    m_Lin.clear();
+    Tree::Expr guardExpr = apply_visitor(*this, std::get<1>(evar));
+    myLin.insert(myLin.end(), m_Lin.begin(), m_Lin.end());
+
+    //boolean
+    m_Lin.clear();
+    Tree::Expr booleanExpr = apply_visitor(*this, std::get<2>(evar));;
+    myLin.insert(myLin.end(), m_Lin.begin(), m_Lin.end());
+
+    m_newVars.push_back
+    (
+      Parser::Equation
+      (
+        std::get<0>(evar),
+        guardExpr,
+        booleanExpr,
+        varExpr
+      )
+    );
+    m_Lin.clear();
+  }
+
+  //visit child E
+  Tree::Expr expr = apply_visitor(*this, e.e);
+  myLin.insert(myLin.end(), m_Lin.begin(), m_Lin.end());
+
+  //we have now fully computed the where clause's L_in
+  w.Lin = myLin;
+
+  //rewrite E to
+  //E @ [d_i <- E_i, Lin_i <- 0] @ [l <- #l + 1]
+  //d_i is [which <- index_d, Lout_i <- #Lout_i]
+  Tree::TupleExpr::TuplePairs odometerDims;
+
+  Tree::TupleExpr::TuplePairs outPairs;
+  //generate the Lout_i pairs
+  for (dimension_index d : m_Lout)
+  {
+    outPairs.push_back(std::make_pair(Tree::DimensionExpr(d),
+      Tree::HashExpr(Tree::DimensionExpr(d), false)));
+  }
+
+  //generate a unique "which" for each dimension
+  for (const auto& v : e.dims)
+  {
+    int next = m_system.nextHiddenDim();
+    w.whichDims.push_back(next);
+    odometerDims.push_back
+    (
+      std::make_pair(Tree::IdentExpr(v.first), apply_visitor(*this, v.second))
+    );
+
+    //generate the dimExpr
+    Tree::TupleExpr::TuplePairs dimTuple
+      {{Tree::DimensionExpr(U"which"), mpz_class(next)}}
+    ;
+
+    dimTuple.insert(dimTuple.end(), outPairs.begin(), outPairs.end());
+
+    //make a new var d = dimval
+    m_newVars.push_back
+    (
+      Parser::Equation(v.first, Tree::Expr(), Tree::Expr(), 
+        Tree::TupleExpr(dimTuple)
+      )
+    );
+  }
+
+  //generate the E which sets L_in dimension to 0
+  for (auto dim : myLin)
+  {
+    odometerDims.push_back(
+      std::make_pair(Tree::DimensionExpr(dim), mpz_class(0)));
+  }
+
+  //increment our own dim
+  Tree::Expr incOwnRaw = Tree::BinaryOpExpr
+    (
+      Tree::BinaryOperator(Tree::ASSOC_LEFT, U"plus", U"+", 0),
+      Tree::HashExpr(Tree::DimensionExpr(label), false),
+      mpz_class(1)
+    );
+
+  Tree::Expr incOwn = apply_visitor(*this, incOwnRaw);
+
+  w.e = Tree::AtExpr
+    (
+      Tree::AtExpr(expr, Tree::TupleExpr(odometerDims)),
+      Tree::TupleExpr({{Tree::DimensionExpr(label), incOwn}})
+    );
+
+  //we return the rewritten E and add the variables to the list of variables
+  //to add to the system
+
+  //also add ourselves to the L_in to return
+  myLin.push_back(label);
+  m_Lin = std::move(myLin);
+
+  //restore L_out
+  m_Lout.pop_back();
+
+  return w;
+}
+
+Tree::Expr
+SemanticTransform::operator()(const Tree::LambdaExpr& e)
+{
+}
+
+Tree::Expr
+SemanticTransform::operator()(const Tree::PhiExpr& e)
+{
+}
+
+}
