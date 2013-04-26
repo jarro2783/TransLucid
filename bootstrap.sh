358eee9a38d122c56b090282f214a94b7262ecca
John Plaice <johnplaice@gmail.com>
2013-04-26 09:12:21 -0400

    Updated gettext to get rid of autoconf warnings. 	modified:   bootstrap.sh 	modified:   configure.ac

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
+echo "Invoking aclocal"
 aclocal -I m4 --force
+echo "Invoking autopoint"
 autopoint --force
 if (test "X`which glibtoolize`" != "X")
 then
-  echo "Using glibtoolize"
-  glibtoolize --copy --force
+  LIBTOOLIZE=glibtoolize
 elif (test "X`which libtoolize`" != "X")
 then
-  echo "Using libtoolize"
-  libtoolize --copy --force
+  LIBTOOLIZE=libtoolize
 else 
   echo "No libtoolize"
   exit 1
 fi
+echo "Invoking $LIBTOOLIZE"
+$LIBTOOLIZE --copy --force
+echo "Invoking autoheader"
 autoheader --force
+echo "Invoking automake"
 automake --add-missing --copy --force
+echo "Invoking autoconf"
 autoconf --force
