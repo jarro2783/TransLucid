8cf2d1e7f09be01f2b0fb1efbced4771613ae115
John Plaice <johnplaice@gmail.com>
2013-04-26 05:47:23 -0400

    On the Mac, libtoolize is called glibtoolize. 	modified:   bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,16 @@
 
 aclocal -I m4 --force
 autopoint --force
-libtoolize --copy --force
+if (test "X`which glibtoolize`" != "X")
+then
+  glibtoolize --copy --force
+elif (test "X`which libtoolize`" != "X")
+then
+  libtoolize --copy --force
+else 
+  echo "No libtoolize"
+  exit 1
+fi
 autoheader --force
 automake --add-missing --copy --force
 autoconf --force
