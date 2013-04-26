2b38b42885bc6af62d95b2d295ff4d7176b1d14f
John Plaice <johnplaice@gmail.com>
2013-04-26 05:54:04 -0400

    Add comments to boostrap's output. 	modified:   bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,9 +4,11 @@ aclocal -I m4 --force
 autopoint --force
 if (test "X`which glibtoolize`" != "X")
 then
+  echo "Using glibtoolize"
   glibtoolize --copy --force
 elif (test "X`which libtoolize`" != "X")
 then
+  echo "Using libtoolize"
   libtoolize --copy --force
 else 
   echo "No libtoolize"
