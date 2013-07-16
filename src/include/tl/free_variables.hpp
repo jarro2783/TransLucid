3bd2259fd99b377008a737110f3cc0c72fe72e86
Jarryd Beck <jarrydb@cse.unsw.edu.au>
2013-07-16 18:15:37 +1000

    missing header

---
+++ src/include/tl/free_variables.hpp
@@ -0,0 +1,58 @@
+/* Finds free variables in an expression.
+   Copyright (C) 2013 Jarryd Beck
+
+This file is part of TransLucid.
+
+TransLucid is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3, or (at your option)
+any later version.
+
+TransLucid is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with TransLucid; see the file COPYING.  If not see
+<http://www.gnu.org/licenses/>.  */
+
+#ifndef TL_FREE_VARIABLES_HPP_INCLUDED
+#define TL_FREE_VARIABLES_HPP_INCLUDED
+
+#include <tl/ast.hpp>
+#include <tl/generic_walker.hpp>
+
+namespace TransLucid
+{
+  //finds the free variables inside an expression, ignoring where clauses
+  class FreeVariables : private GenericTreeVisitor<FreeVariables>
+  {
+    public:
+
+    typedef void result_type;
+
+    std::set<u32string>
+    findFree(const Tree::Expr& e);
+
+    template <typename T>
+    result_type
+    operator()(const T&) const
+    {
+      return result_type();
+    }
+
+    result_type
+    operator()(const Tree::IdentExpr&);
+
+    result_type
+    operator()(const Tree::WhereExpr&);
+
+    private:
+
+    std::set<u32string> m_free;
+  };
+
+}
+
+#endif
